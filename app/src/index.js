404c32feceaa575d4b0450199b65de52521e2188
Ruslan Rasulov <ruslanrasulov31@gmail.com>
2018-09-15 14:18:47 +0400

    configured react app environment

---
+++ app/src/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app/App.jsx';
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root'));
---
+++ app/webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const outputPath = path.resolve(__dirname, 'dist');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: outputPath,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: '/node_modules/',
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+            path: outputPath
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: outputPath,
+        port: 3000,
+        inline: true,
+        hot: true
+    }
+}
+
+module.exports = config;
